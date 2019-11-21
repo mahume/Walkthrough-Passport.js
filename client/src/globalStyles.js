433a058b0e677051cf3b93122730502c3b88e4c1
mahume <michael.a.hume@gmail.com>
2019-11-20 17:31:20 -0700

    Install styled-components & react router. Setup global styles

---
+++ client/src/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components/macro";
+
+export default GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    margin: 0;
+  }
+`;
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
+import GlobalStyles from './globalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <GlobalStyles />
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
