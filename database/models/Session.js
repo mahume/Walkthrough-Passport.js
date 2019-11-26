37b312f4659474ad1a0a8570b354cf8b4897d0a6
mahume <michael.a.hume@gmail.com>
2019-11-26 15:40:45 -0700

    Creating Session model

---
+++ database/models/Session.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const SessionSchema = new Schema({
+  userId: {
+    type: Number,
+    default: -1,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now(),
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+const Session = mongoose.model('Session', SessionSchema);
+
+module.exports = Session;
